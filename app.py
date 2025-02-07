8e0d3b1e318a4e1e5040f069ea2110ea3779a0ac
Vo Minh Tri <vo.minh-tri1@hdsaison.com.vn>
2025-02-07 15:05:10 +0700

    update

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def __get_images(request):
     return [front, back]
 
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app.run()
